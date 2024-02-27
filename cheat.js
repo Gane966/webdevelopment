e314d69971f0cb71c859358298958371429f6e8d
Gane@966 <reddyganeshgajula@gmail.com>
2024-02-27 12:10:38 +0530

    updated

---
+++ cheat.js
@@ -0,0 +1,49 @@
+//x = 'ganesh is in vidatt company'
+//console.log(...x)
+
+let date = new Date()
+
+let monthName;
+
+switch (date.getMonth()+1) {
+  case 1:
+    monthName = "January";
+    break;
+  case 2:
+    monthName = "February";
+    break;
+  case 3:
+    monthName = "March";
+    break;
+  case 4:
+    monthName = "April";
+    break;
+  case 5:
+    monthName = "May";
+    break;
+  case 6:
+    monthName = "June";
+    break;
+  case 7:
+    monthName = "July";
+    break;
+  case 8:
+    monthName = "August";
+    break;
+  case 9:
+    monthName = "September";
+    break;
+  case 10:
+    monthName = "October";
+    break;
+  case 11:
+    monthName = "November";
+    break;
+  case 12:
+    monthName = "December";
+    break;
+  default:
+    monthName = "Unknown";
+}
+
+console.log(monthName); // Output: "present month"
