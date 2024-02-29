98c4ca2edddf33e7be8393b158029a0699a77f9e
Gane966 <117095541+Gane966@users.noreply.github.com>
2024-02-29 18:13:42 +0530

    Javascript 27 02 (#2)
    * updated
    
    * Added a java script program

--- cheat.js
+++ cheat.js
@@ -1,49 +1,71 @@
 //x = 'ganesh is in vidatt company'
 //console.log(...x)
 
-let date = new Date()
 
-let monthName;
+//let date = new Date()
+//
+//let monthName;
+//
+//switch (date.getMonth()+1) {
+//  case 1:
+//    monthName = "January";
+//    break;
+//  case 2:
+//    monthName = "February";
+//    break;
+//  case 3:
+//    monthName = "March";
+//    break;
+//  case 4:
+//    monthName = "April";
+//    break;
+//  case 5:
+//    monthName = "May";
+//    break;
+//  case 6:
+//    monthName = "June";
+//    break;
+//  case 7:
+//    monthName = "July";
+//    break;
+//  case 8:
+//    monthName = "August";
+//    break;
+//  case 9:
+//    monthName = "September";
+//    break;
+//  case 10:
+//    monthName = "October";
+//    break;
+//  case 11:
+//    monthName = "November";
+//    break;
+//  case 12:
+//    monthName = "December";
+//    break;
+//  default:
+//    monthName = "Unknown";
+//}
+//
+//console.log(monthName); // Output: "present month"
 
-switch (date.getMonth()+1) {
-  case 1:
-    monthName = "January";
-    break;
-  case 2:
-    monthName = "February";
-    break;
-  case 3:
-    monthName = "March";
-    break;
-  case 4:
-    monthName = "April";
-    break;
-  case 5:
-    monthName = "May";
-    break;
-  case 6:
-    monthName = "June";
-    break;
-  case 7:
-    monthName = "July";
-    break;
-  case 8:
-    monthName = "August";
-    break;
-  case 9:
-    monthName = "September";
-    break;
-  case 10:
-    monthName = "October";
-    break;
-  case 11:
-    monthName = "November";
-    break;
-  case 12:
-    monthName = "December";
-    break;
-  default:
-    monthName = "Unknown";
+
+
+//prime number program using java script
+const a = 100
+var output = []
+for(let i =1;i<=a;i++){
+    let isprime = true
+    for(let j = 2;j<i-1/2;j++){
+        if(i%j===0){
+        isprime = false
+        break
+        }
+    }
+    if(isprime){
+        output.push(i)
+    }
+
 }
 
-console.log(monthName); // Output: "present month"
+console.log(output.length)
