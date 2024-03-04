e28ca018acee075640fa7bd85de9501452b0268e
Gane@966 <reddyganeshgajula@gmail.com>
2024-03-04 19:31:53 +0530

    up

--- cheat.js
+++ cheat.js
@@ -50,22 +50,34 @@
 
 
 
-//prime number program using java script
-const a = 100
-var output = []
-for(let i =1;i<=a;i++){
-    let isprime = true
-    for(let j = 2;j<i-1/2;j++){
-        if(i%j===0){
-        isprime = false
-        break
-        }
-    }
-    if(isprime){
-        output.push(i)
-    }
+//@prime number program using java script
+//const a = 100
+//var output = []
+//for(let i =1;i<=a;i++){
+//    let isprime = true
+//    for(let j = 2;j<i-1/2;j++){
+//        if(i%j===0){
+//        isprime = false
+//        break
+//        }
+//    }
+//    if(isprime){
+//        output.push(i)
+//    }
+//
+//}
+//
+//console.log(output.length)
 
-}
 
-console.log(output.length)
 
+// @writing if condition in a single line
+let a = 10
+let b = 20
+
+console.log(a===10? (a+b==130? true: false) : false)
+//
+//let x = 5;
+//let y = 10;
+//
+//(x > 0 ? (y < 15 ? console.log("Both conditions are true") : console.log("Condition 1 is true, Condition 2 is false")) : console.log("Condition 1 is false"));
